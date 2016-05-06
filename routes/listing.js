363c7ca804efcda126a3fafe96b37a9a78b192a2
scootysn <scootysn@users.noreply.github.com>
2016-05-05 17:21:25 -0700

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -4,7 +4,7 @@ exports.getuserlisting = function(req,res,userid, l, o) {
 	var limit = l;
 	var offset = o;
 	
-	var getrecords = "select (select count(r.startdate) from spot as s inner join reservation as r on s.userid = r.userid and s.spotid = r.spotid where s.userid = 21) as Totalrows, r.startdate, r.enddate, r.starttime, r.endtime, concat(s.addressline1,' ',s.addressline2) as address, r.status from spot as s inner join reservation as r on s.userid = r.userid and s.spotid = r.spotid where s.userid = "+userid+" LIMIT "+limit+" OFFSET "+offset+";";
+	var getrecords = "select (select count(r.startdate) from spot as s inner join reservation as r on s.userid = r.userid and s.spotid = r.spotid where s.userid = "+userid+") as Totalrows, r.startdate, r.enddate, r.starttime, r.endtime, concat(s.addressline1,' ',s.addressline2) as address, r.status from spot as s inner join reservation as r on s.userid = r.userid and s.spotid = r.spotid where s.userid = "+userid+" LIMIT "+limit+" OFFSET "+offset+";";
 	console.log("Query is:" + getrecords);
 
 	// Calling the fetch method using mysql module
